d61e7a0e6e53b4230890f1a9be926935b09336da
yvoloshchuk <yaroslav.voloshchuk@grammarly.com>
2014-10-19 19:38:25 +0300

    seed gh-pages

---
+++ components/paper-player/README.md
@@ -0,0 +1,8 @@
+paper-player
+================
+
+See the [component page](http://wenqeer.github.io/paper-player) for more information.
+
+## Getting Started
+
+We've put together a [guide for paper-player](http://www.polymer-project.org/docs/start/reusableelements.html) to help get you rolling.
---
+++ components/paper-player/paper-player.js
@@ -0,0 +1,81 @@
+(function () {
+  'use strict'
+
+  Polymer({
+    ready: function() {
+      this.tracks = [
+        {
+          title: 'Linus Young - Home',
+          src: 'http://cs7-4v4.vk.me/p18/6102920be909b4.mp3'
+        },
+        {
+          title: 'Linus Young - Crystal Ball',
+          src: 'http://cs7-3v4.vk.me/p18/33a06f5f4a8fe9.mp3'
+        }
+      ]
+
+      this.player = this.$.playerContainer
+      this.current = this.tracks[0]
+      this.played = !this.player.paused
+    },
+    tracksChanged: function(val) {
+      console.log(val)
+    },
+    setTrack: function(e, d, s) {
+      this.current = s.templateInstance.model.track
+      return this.play()
+    },
+    seekTo: function(e, d, s) {
+      this.player.currentTime = s.value
+    },
+    togglePlay: function() {
+      this.played ? this.pause() : this.play()
+    },
+    trackEnded: function() {
+      var pos = this.tracks.indexOf(this.current) + 1
+      pos = this.tracks[pos] ? pos : 0
+      this.current = this.tracks[pos]
+      this.play()
+    },
+    durationChange: function() {
+      this.trackDuration = this.player.duration
+    },
+    timeUpdate: function() {
+      this.currentTrackDurration = this.player.currentTime
+    },
+    progressUpdate: function() {
+      var buf = this.player.buffered,
+        len = buf.length
+      if (len) {
+        this.downloaded = buf.end(len - 1)
+      }
+    },
+    toTime: function(val) {
+      var m = Math.round(val / 60),
+        s = Math.round(val % 60)
+      if (s < 10) s = '0' + s
+      return [m, s].join(':')
+    },
+    printJson: {
+      toDOM: function(value) {
+        return value && JSON.stringify(value, null, '\t')
+      },
+      toModel: function(value) {
+        return value && JSON.parse(value)
+      }
+    },
+    play: function() {
+      this.async(function() {
+        this.player.play()
+      })
+      this.played = true
+      return this
+    },
+    pause: function() {
+      this.player.pause()
+      this.played = false
+      return this
+    }
+  })
+
+})()
